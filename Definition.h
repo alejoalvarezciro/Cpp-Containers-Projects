ef705f34822d70f0e74783760aa4702ee133dd6c
Alejandro <alejoalvarezciro@gmail.com>
2026-09-12 13:08:36 -0400

    Adding rule of 5 to Definition

--- Definition.h
+++ Definition.h
@@ -12,6 +12,13 @@ public:
 	//Constructors
 	Definition(const char* ID, const char* def);
 
+	//Rule of 5
+	~Definition() = default;
+	Definition(const Definition& other) = default;
+	Definition& operator=(const Definition& other) = default;
+	Definition(Definition&& other) noexcept = default;
+	Definition& operator=(Definition&& other) noexcept = default;
+
 	//Getters
 	const CharMatrix& getWord() const;
 	const CharMatrix& getResult() const;
