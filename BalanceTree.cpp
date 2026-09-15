54d9014dce0d0c85280503c38785079d09f1e457
Alejandro <alejoalvarezciro@gmail.com>
2026-09-14 22:51:01 -0400

    Add rule of 5, but not completed or tested yet. Aswell as some functions to help with deleting and copying.

--- BalanceTree.cpp
+++ BalanceTree.cpp
@@ -1,10 +1,63 @@
 #include "BalanceTree.h"
 
-void BalanceTree::clearAll(Node* pNode) {
+/////////////////////////
+////Private Functions////
+/////////////////////////
+void BalanceTree::clearAll(const Node* pNode) {
 	if (pNode != nullptr)
 	{
 		clearAll(pNode->left);
 		clearAll(pNode->right);
 		delete pNode;
 	}
+}
+
+void BalanceTree::copyFathersAndSons(const Node* pNodeCopy, const Node* pNodeOriginal) {
+	if (pNodeOriginal != nullptr)
+	{
+		
+	}
+}
+
+////////////////////
+////Constructors////
+////////////////////
+BalanceTree::BalanceTree() :
+	root(nullptr), size(0) {
+}
+
+/////////////////
+////Rule of 5////
+/////////////////
+BalanceTree::~BalanceTree() {
+	clearAll(root);
+}
+BalanceTree::BalanceTree(const BalanceTree& other) :
+	root(nullptr), size(0) {
+	
+}
+BalanceTree& BalanceTree::operator=(const BalanceTree& other) {
+	if (&other == this)
+	{
+		return *this;
+	}
+	clearAll(root);
+	return *this;
+}
+BalanceTree::BalanceTree(BalanceTree&& other) noexcept :
+	root(other.root), size(other.size) {
+	other.root = nullptr;
+	other.size = 0;
+}
+BalanceTree& BalanceTree::operator=(BalanceTree&& other) noexcept {
+	if (&other == this)
+	{
+		return *this;
+	}
+	clearAll(root);
+	root = other.root;
+	size = other.size;
+	other.root = nullptr;
+	other.size = 0;
+	return *this;
 }
--- BalanceTree.h
+++ BalanceTree.h
@@ -4,23 +4,43 @@ class BalanceTree
 private:
 
 	struct Node {
-		int weight;
+		size_t weight;
 		int id;
 		Node* left;
 		Node* right;
 		Node(const int& pID, Node* pLeft = nullptr, Node* pRight = nullptr) :
 			id(pID), weight(0), left(pLeft), right(pRight) {
 		}
+
+		void updateWeight() {
+			if (left != nullptr)
+			{
+				weight = (weight > left->weight + 1) ? weight : left->weight + 1;
+			}
+			if (right != nullptr)
+			{
+				weight = (weight > right->weight + 1) ? weight : right->weight + 1;
+			}
+		};
 	};
 
 	Node* root;
 	size_t size;
 
-	void clearAll(Node* pNode);
+	void clearAll(const Node* pNode);
+	void copyFathersAndSons(const Node* pNodeCopy, const Node* pNodeOriginal);
 
 public:
 
+	//Constructors
+	BalanceTree();
 
+	//Rule of 5
+	~BalanceTree();
+	BalanceTree(const BalanceTree& other);
+	BalanceTree& operator=(const BalanceTree& other);
+	BalanceTree(BalanceTree&& other) noexcept;
+	BalanceTree& operator=(BalanceTree&& other) noexcept;
 
 };
 
